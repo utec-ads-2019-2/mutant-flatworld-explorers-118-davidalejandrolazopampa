df7c7e9489c01574d06d07d1b813fef913edf2e7
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2019-09-18 18:01:24 -0500

    ....

---
+++ cmake-build-debug/CMakeFiles/mutant_flatworld_explorers_118_davidalejandrolazopampa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mutant_flatworld_explorers_118_davidalejandrolazopampa.dir/main.cpp.obj"
+  "mutant_flatworld_explorers_118_davidalejandrolazopampa.pdb"
+  "mutant_flatworld_explorers_118_davidalejandrolazopampa.exe"
+  "mutant_flatworld_explorers_118_davidalejandrolazopampa.exe.manifest"
+  "mutant_flatworld_explorers_118_davidalejandrolazopampa.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mutant_flatworld_explorers_118_davidalejandrolazopampa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
