8720130c08b32c4e5131e4c90dee5c3d1c29a5e5
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2019-09-18 18:42:43 -0500

    Accept

--- main.cpp
+++ main.cpp
@@ -6,63 +6,62 @@
 #include <string>
 
 using namespace std;
-
-char directions[] = {'N', 'E', 'S', 'W'};
-int xChange[] = {0, 1, 0, -1};
-int yChange[] = {1, 0, -1, 0};
-
+//Segun mande el documento
+char pink[] = {'N', 'E', 'S', 'W'};
+int xx[] = {0, 1, 0, -1};
+int yy[] = {1, 0, -1, 0};
 
 int main()
 {
-    int xSize, ySize;
+    //Inicio fin
+    int xSize, ySize,x, y;
+    char inicio;
+    string orders;
     cin >> xSize >> ySize;
 
     vector<vector<bool> > scent(ySize + 1, vector<bool> (xSize + 1, false));
-    //Inicio fin
-    int x, y;
-    char startDirection;
-    std::string orders;
 
-    while (cin >> x >> y >> startDirection >> orders)
+    while (cin >> x >> y >> inicio >> orders)
     {
-        int direction;
+        int array;
 
         for (int i = 0; i < 4; ++i)
-            if (startDirection == directions[i])
-                direction = i;
+            if (inicio == pink[i])
+                array = i;
 
-        bool died = false;
-
+        bool punto = false;
 
         for (int i = 0; i < orders.size(); ++i)
         {
 
             if (orders[i] == 'R')
-                direction = (direction + 1) % 4;
+                array = (array + 1) % 4;
 
             else if (orders[i] == 'L')
-                direction = (direction + 3) % 4;
+                array = (array + 3) % 4;
 
             else
             {
                 //posicion
-                if (x + xChange[direction] == -1 || x + xChange[direction] == xSize + 1 || y + yChange[direction] == -1 || y + yChange[direction] == ySize + 1)
+                if ((x + xx[array] == -1) || (x + xx[array] == xSize + 1 ) || (y + yy[array] == -1) || (y + yy[array] == ySize + 1))
                 {
                     if (!scent[y][x])
                     {
-                        scent[y][x] = died = true;
+                        scent[y][x] = punto = true;
                         break;
                     }
                 }
 
                 else
                 {
-                    x += xChange[direction];
-                    y += yChange[direction];
+                    x += xx[array];
+                    y += yy[array];
                 }
             }
         }
 
-        cout << x << ' ' << y << ' ' << directions[direction] << (died ? " LOST" : "") << '\n';
+        cout << x << ' ' << y << ' ' << pink[array] << (punto ?" LOST": "") << '\n';
     }
+
+
 }
